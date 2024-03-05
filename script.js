f1d2ddb40bf9a133f9d94f0403bb446091ed4519
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-05 19:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstChar(text) {
   // your code here
-	text = text.trim();
-	return text.charAt(0);
+	let trimText = text.trim();
+	return trimText.charAt(0);
 }
 
 // Do not change the code below
